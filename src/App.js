05fa79d4172598e70bd8ecd7e07113d4b5b28e21
Nilesh Mukherjee <nileshmukherjee1855@gmail.com>
2024-03-07 23:58:21 +0530

    preload font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Navbar from './components/Navbar/Navbar'
-import Intro from './components/Intro/Intro'
-import Skills from './components/Skills/Skills'
+import Navbar from './components/Navbar/Navbar';
+import Intro from './components/Intro/Intro';
+import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
@@ -9,16 +10,28 @@ import Interests from './components/Interests/Interests';
 import About from './components/About/About';
 
 function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    document.fonts.ready.then(() => {
+      setFontLoaded(true);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <Navbar />
-      <Intro />
-      <Interests />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+    <div className={`App ${fontLoaded ? 'font-loaded' : ''}`}>
+      {fontLoaded && (
+        <>
+          <Navbar />
+          <Intro />
+          <Interests />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
